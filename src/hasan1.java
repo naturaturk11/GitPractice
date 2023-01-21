1421ba86688f9c1e1bb56c33e5cad44d69c1af8d
naturaturk11 <112637566+naturaturk11@users.noreply.github.com>
2023-01-22 00:35:53 +0300

    Update hasan1.java
    some more info given

--- src/hasan1.java
+++ src/hasan1.java
@@ -1,5 +1,6 @@
 public class hasan1 {
     public static void main(String[] args) {
         System.out.println("I need to recap the git once more");
+        //more detailed information should be given to the main structure
     }
 }
